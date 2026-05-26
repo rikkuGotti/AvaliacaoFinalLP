36e4d43b630528dc257930992efcd49c5677bc1d
GGotti <enricopimentel2006@gmail.com>
2026-05-26 02:34:42 -0300

    Incio do desenvolvimento da galeria, e foi adicionado 2 efeitos vizuais com JS, Alteração de tema do site e filtro

--- js/script.js
+++ js/script.js
@@ -11,3 +11,31 @@ const btnTema = document.querySelector('#btnTema');
 btnTema.addEventListener('click', function() {
     document.body.classList.toggle('tema-escuro');
 });
+
+const botoes = document.querySelectorAll(".filtros .btn");
+const cards = document.querySelectorAll(".row article");
+
+botoes.forEach(botao => {
+  botao.addEventListener("click", () => {
+    let categoria = botao.textContent.toLowerCase();
+
+    // Remove acentos
+    categoria = categoria.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+    // Remove plural (s no final)
+    if (categoria.endsWith("s")) {
+      categoria = categoria.slice(0, -1);
+    }
+
+    cards.forEach(card => {
+      if (botao.textContent.toLowerCase() === "todos") {
+        // Mostra todos os cards
+        card.style.display = "block";
+      } else if (card.classList.contains(`categoria-${categoria}`)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
