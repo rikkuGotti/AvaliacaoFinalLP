b9e8c83c523cba4081410ba1e388477f41001619
GGotti <enricopimentel2006@gmail.com>
2026-05-26 01:49:26 -0300

    Home finalizada + foi adicionada a opção de alterar tema do site com css e js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*
+AF - Avaliação Final - Linguagens de Programação
+Tema do Projeto: Nome do tema escolhido
+Aluno 1: Enrico Pimentel Gotti Encarnação - RA: 250779
+Aluno 2: João Vitor Vieira Skierzynski  - RA: 250964
+Professor: Abimael de Oliveira
+Data de entrega: 07/06
+*/
+const btnTema = document.querySelector('#btnTema');
+
+btnTema.addEventListener('click', function() {
+    document.body.classList.toggle('tema-escuro');
+});
